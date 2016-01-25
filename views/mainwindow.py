4be00be565e2034f9b2a77957b4e0784f560c13a
Yao Sun <mr.sun.yao@gmail.com>
2016-01-24 21:08:57 -0500

    test

--- views/mainwindow.py
+++ views/mainwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'C:\Users\Yao\Documents\GitHub\batchy\views\mainwindow.ui'
 #
-# Created: Sun Jan 24 16:05:36 2016
+# Created: Sun Jan 24 21:08:50 2016
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,11 +12,11 @@ from PySide import QtCore, QtGui
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(410, 280)
+        MainWindow.resize(410, 348)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.startButton = QtGui.QPushButton(self.centralwidget)
-        self.startButton.setGeometry(QtCore.QRect(290, 210, 51, 23))
+        self.startButton.setGeometry(QtCore.QRect(310, 250, 41, 23))
         self.startButton.setObjectName("startButton")
         self.plusButton = QtGui.QPushButton(self.centralwidget)
         self.plusButton.setGeometry(QtCore.QRect(380, 210, 21, 23))
@@ -24,13 +24,24 @@ class Ui_MainWindow(object):
         self.minusButton = QtGui.QPushButton(self.centralwidget)
         self.minusButton.setGeometry(QtCore.QRect(350, 210, 21, 23))
         self.minusButton.setObjectName("minusButton")
-        self.videoProgressBar = QtGui.QProgressBar(self.centralwidget)
-        self.videoProgressBar.setGeometry(QtCore.QRect(10, 210, 281, 23))
-        self.videoProgressBar.setProperty("value", 24)
-        self.videoProgressBar.setObjectName("videoProgressBar")
         self.videoListWidget = QtGui.QListWidget(self.centralwidget)
         self.videoListWidget.setGeometry(QtCore.QRect(10, 10, 391, 192))
         self.videoListWidget.setObjectName("videoListWidget")
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(360, 250, 41, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.groupBox = QtGui.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(10, 230, 291, 51))
+        self.groupBox.setObjectName("groupBox")
+        self.configComboBox = QtGui.QComboBox(self.groupBox)
+        self.configComboBox.setGeometry(QtCore.QRect(10, 20, 171, 22))
+        self.configComboBox.setObjectName("configComboBox")
+        self.configNewBtn = QtGui.QPushButton(self.groupBox)
+        self.configNewBtn.setGeometry(QtCore.QRect(190, 20, 41, 23))
+        self.configNewBtn.setObjectName("configNewBtn")
+        self.configEditBtn = QtGui.QPushButton(self.groupBox)
+        self.configEditBtn.setGeometry(QtCore.QRect(240, 20, 41, 23))
+        self.configEditBtn.setObjectName("configEditBtn")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 410, 21))
@@ -64,6 +75,10 @@ class Ui_MainWindow(object):
         self.startButton.setText(QtGui.QApplication.translate("MainWindow", "Start", None, QtGui.QApplication.UnicodeUTF8))
         self.plusButton.setText(QtGui.QApplication.translate("MainWindow", "+", None, QtGui.QApplication.UnicodeUTF8))
         self.minusButton.setText(QtGui.QApplication.translate("MainWindow", "-", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "Stop", None, QtGui.QApplication.UnicodeUTF8))
+        self.groupBox.setTitle(QtGui.QApplication.translate("MainWindow", "Config", None, QtGui.QApplication.UnicodeUTF8))
+        self.configNewBtn.setText(QtGui.QApplication.translate("MainWindow", "New", None, QtGui.QApplication.UnicodeUTF8))
+        self.configEditBtn.setText(QtGui.QApplication.translate("MainWindow", "Edit", None, QtGui.QApplication.UnicodeUTF8))
         self.menuFile.setTitle(QtGui.QApplication.translate("MainWindow", "File", None, QtGui.QApplication.UnicodeUTF8))
         self.menuAbout.setTitle(QtGui.QApplication.translate("MainWindow", "About", None, QtGui.QApplication.UnicodeUTF8))
         self.actionOpen.setText(QtGui.QApplication.translate("MainWindow", "Open", None, QtGui.QApplication.UnicodeUTF8))
